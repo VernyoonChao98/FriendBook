9cde0c7a03b71d0b4436cc88f22a870077e10d53
Vernyoon Chao <vernchao510@gmail.com>
2022-06-15 22:45:30 -0700

    links to my github and linked in

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginForm from "../auth/LoginForm";
 import SignUpModal from "../auth/SignUpModal";
+import { ExternalLink } from "react-external-link";
 
 function SplashPage() {
   return (
@@ -19,7 +20,32 @@ function SplashPage() {
           </div>
         </div>
       </div>
-      <footer className="footer"></footer>
+      <footer className="footer">
+        {/* <ExternalLink href="https://www.linkedin.com/in/vernyoon-chao-783494123/">
+          Portfolio
+        </ExternalLink> */}
+        <ExternalLink
+          className="external_link"
+          href="https://www.linkedin.com/in/vernyoon-chao-783494123/"
+        >
+          <div className="linkedIn"></div>
+          LinkedIn
+        </ExternalLink>
+        <ExternalLink
+          className="external_link"
+          href="https://github.com/VernyoonChao98"
+        >
+          <div className="github"></div>
+          Github
+        </ExternalLink>
+        <ExternalLink
+          className="external_link"
+          href="https://github.com/VernyoonChao98/FriendBook"
+        >
+          <div className="github"></div>
+          Project Repo
+        </ExternalLink>
+      </footer>
     </div>
   );
 }
